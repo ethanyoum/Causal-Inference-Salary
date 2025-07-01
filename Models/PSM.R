87aff363b890698661cc06392d086991483e23cd
Ethan Youm <sy327@duke.edu>
2025-07-01 00:47:19 -0400

    Create PSM.R

---
+++ Models/PSM.R
@@ -0,0 +1,56 @@
+data$work_setting <- as.factor(ifelse(data$work_setting == "Remote", 1, 0))  # Treatment Variable (Remote = 1, In-person = 0)
+data$job_title <- as.factor(data$job_title)  # Data Analyst vs Data Engineer
+data$experience_level <- as.factor(data$experience_level)  # Entry-level, Mid-level, Senior, Executive
+data$company_size <- as.factor(data$company_size)  # Small (S), Medium (M), Large (L)
+data$employment_type <- as.factor(data$employment_type)  # Full-time vs Contract
+data$company_location <- as.factor(data$company_location)  # US locations
+
+# Run Propensity Score Matching Model
+match_out <- matchit(work_setting ~ job_title + experience_level + company_size + employment_type, 
+                     data = data, method = "full", distance = "logit", ratio=2, estimand = "ATT")
+summary(match_out)
+
+# Extract Matched Data
+matched_data <- match.data(match_out)
+
+# Visualizing Balance
+plot(match_out, type = "jitter", interactive = FALSE)
+plot(match_out, type = "hist")
+
+psm_effect <- lm(salary_in_usd ~ work_setting * job_title, data = matched_data)
+summary(psm_effect)
+
+t.test(matched_data$salary_in_usd ~ matched_data$work_setting)
+
+ATT.model <- lm(salary ~ work_setting * job_title + experience_level + company_size + 
+                employment_type, data = matched_data, weights = matched_data$weights)
+
+summary(ATT.model)
+
+match_out_ATC <- matchit(work_setting ~ job_title + experience_level + company_size + employment_type, 
+                     data = data, method = "full", distance = "logit", ratio=2, estimand = "ATT")
+summary(match_out_ATC)
+
+matched_data_ATC <- match.data(match_out_ATC)
+
+# Run regression on matched data (ATC)
+ATC.model <- lm(salary ~ work_setting * job_title + experience_level + 
+                company_size + employment_type, 
+                data = matched_data_ATC, weights = matched_data_ATC$weights)
+
+# Display summary of the model
+summary(ATC.model)
+
+match_out_ATE <- matchit(work_setting ~ job_title + experience_level + company_size + employment_type, 
+                     data = data, method = "full", distance = "logit", ratio=2, estimand = "ATE")
+summary(match_out_ATE)
+
+matched_data_ATE <- match.data(match_out_ATE)
+
+# Run regression on matched data (ATC)
+ATE.model <- lm(salary ~ work_setting * job_title + experience_level + 
+                company_size + employment_type, 
+                data = matched_data_ATC, weights = matched_data_ATE$weights)
+
+# Display summary of the model
+summary(ATE.model)
